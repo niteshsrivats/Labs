7d15e6e254ef21eff03aa6f5572863111d8860f4
Nitesh Srivats <niteshsrivats.k@gmail.com>
2019-10-17 06:21:15 +0530

    Reverted to old file version

--- 5th Semester/ADA/Sixth Class/Queens.py
+++ 5th Semester/ADA/Sixth Class/Queens.py
@@ -9,7 +9,7 @@ def is_safe(matrix, row, col, size):
         if matrix[i][col] == 1:
             return False
 
-    for i in range(1, min(row, size - col)):
+    for i in range(1, min(row + 1, size - col)):
         if matrix[row - i][col + i] == 1:
             return False
 
@@ -19,26 +19,16 @@ def is_safe(matrix, row, col, size):
     return True
 
 
-solutions = list()
 def queen(row=0):
-    global matrix
-    flag = False
     for i in range(size):
         if is_safe(matrix, row, i, size):
             matrix[row][i] = 1
             if row == size - 1:
-                solutions.append(matrix.copy())
-                flag = True
+                return True
             if queen(row + 1):
-                if row == 0:
-                    matrix = np.zeros((size, size))
-                else:
-                    return True
+                return True
             matrix[row][i] = 0
-    return flag
 
 
 queen()
-for solution in solutions:
-    print(solution)
-print(len(solutions))
+print(matrix)
