f61c77d04ce04b1c19c6e6da0383b0f27a78b264
Nitesh Srivats <niteshsrivats.k@gmail.com>
2019-10-21 22:20:38 +0530

    Added Fifth Lab

---
+++ 5th Semester/IOT/Fifth Lab/Moisture.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+const Servo ms;
+const int motor = 9;
+const int sensorPin = A0;
+int sensorValue;
+
+void setup() {
+  pinMode(motor, OUTPUT);
+  ms.attach(motor);
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT);
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  Serial.println (sensorValue);
+  if (sensorValue > 750) {
+    for (int pos = 0; pos <= 180; pos += 1) { 
+      ms.write(pos);            
+      delay(30);                       
+    }
+    for (int pos = 180; pos >= 0; pos -= 1) { 
+      ms.write(pos);         
+      delay(30);                    
+    }
+  }
+  delay (1000);
+}
