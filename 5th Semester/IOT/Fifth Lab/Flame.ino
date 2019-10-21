1c9777a207125eef8c9d0237e61cfac6e8957264
Nitesh Srivats <niteshsrivats.k@gmail.com>
2019-10-21 22:22:42 +0530

    Refactored Code

--- 5th Semester/IOT/Fifth Lab/Flame.ino
+++ 5th Semester/IOT/Fifth Lab/Flame.ino
@@ -2,19 +2,24 @@ const int led = 3;
 const int buzzer = 2;
 const int sensorPin = A0;
 
-void setup() {
+void setup()
+{
   pinMode(led, OUTPUT);
   pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop() {
-  if (analogRead(sensorPin) < 100) {
+void loop()
+{
+  if (analogRead(sensorPin) < 100)
+  {
     Serial.println("Fire hazard.");
     digitalWrite(led, HIGH);
     digitalWrite(buzzer, HIGH);
     delay(500);
-  } else {
+  }
+  else
+  {
     digitalWrite(led, LOW);
     digitalWrite(buzzer, LOW);
   }
--- 5th Semester/IOT/Fifth Lab/Moisture.ino
+++ 5th Semester/IOT/Fifth Lab/Moisture.ino
@@ -4,25 +4,30 @@ const int motor = 9;
 const int sensorPin = A0;
 int sensorValue;
 
-void setup() {
+void setup()
+{
   pinMode(motor, OUTPUT);
   ms.attach(motor);
   Serial.begin(9600);
   pinMode(sensorPin, INPUT);
 }
 
-void loop() {
+void loop()
+{
   sensorValue = analogRead(sensorPin);
-  Serial.println (sensorValue);
-  if (sensorValue > 750) {
-    for (int pos = 0; pos <= 180; pos += 1) { 
-      ms.write(pos);            
-      delay(30);                       
+  Serial.println(sensorValue);
+  if (sensorValue > 750)
+  {
+    for (int pos = 0; pos <= 180; pos += 1)
+    {
+      ms.write(pos);
+      delay(30);
     }
-    for (int pos = 180; pos >= 0; pos -= 1) { 
-      ms.write(pos);         
-      delay(30);                    
+    for (int pos = 180; pos >= 0; pos -= 1)
+    {
+      ms.write(pos);
+      delay(30);
     }
   }
-  delay (1000);
+  delay(1000);
 }
