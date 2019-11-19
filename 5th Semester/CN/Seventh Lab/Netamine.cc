5a84b2c33474b830625f0dbb7e7f9dcbc95c60a2
Nitesh Srivats <niteshsrivats.k@gmail.com>
2019-11-19 23:00:15 +0530

    Corrected netamine

--- 5th Semester/CN/Seventh Lab/Netamine.cc
+++ 5th Semester/CN/Seventh Lab/Netamine.cc
@@ -1,106 +1,71 @@
+
 #include "ns3/core-module.h"
 #include "ns3/csma-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
-#include "ns3/netanim-module.h"
+#include "ns3/animation-interface.h"
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("SecondLabProgram");
-
 int main(int argc, char *argv[])
 {
-  bool verbose = true;
-  uint32_t nCsma = 2;
 
-  CommandLine cmd;
-  cmd.AddValue("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
-  cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
+  LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-  cmd.Parse(argc, argv);
-
-  if (verbose)
-  {
-    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
-  }
-
-  nCsma = nCsma == 0 ? 1 : nCsma;
-
-  NodeContainer p2pNodes1, p2pNodes2;
-  p2pNodes1.Create(2);
-  p2pNodes2.Create(2);
-
-  NodeContainer csmaNodes;
-  csmaNodes.Add(p2pNodes1.Get(1));
-  csmaNodes.Add(p2pNodes2.Get(0));
-  csmaNodes.Create(nCsma);
+  NodeContainer p2pNodes, csmaNodes;
+  p2pNodes.Create(2);
+  csmaNodes.Add(p2pNodes.Get(1));
+  csmaNodes.Create(2);
 
   PointToPointHelper pointToPoint;
+  CsmaHelper csma;
   pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
   pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
-
-  NetDeviceContainer p2pDevices1, p2pDevices2;
-  p2pDevices1 = pointToPoint.Install(p2pNodes1);
-  p2pDevices2 = pointToPoint.Install(p2pNodes2);
-
-  CsmaHelper csma;
   csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
-  csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
+  csma.SetChannelAttribute("Delay", TimeValue(MilliSeconds(5)));
 
-  NetDeviceContainer csmaDevices;
+  NetDeviceContainer p2pDevices, csmaDevices;
+  p2pDevices = pointToPoint.Install(p2pNodes);
   csmaDevices = csma.Install(csmaNodes);
 
   InternetStackHelper stack;
-  stack.Install(p2pNodes1.Get(0));
+  stack.Install(p2pNodes.Get(0));
   stack.Install(csmaNodes);
-  stack.Install(p2pNodes2.Get(1));
 
   Ipv4AddressHelper address;
+  Ipv4InterfaceContainer p2pInterfaces, csmaInterfaces;
   address.SetBase("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer p2pInterfaces1;
-  p2pInterfaces1 = address.Assign(p2pDevices1);
-
-  address.SetBase("10.1.2.0", "255.255.255.0");
-  Ipv4InterfaceContainer csmaInterfaces;
+  p2pInterfaces = address.Assign(p2pDevices);
+  address.SetBase("20.1.2.0", "255.255.255.0");
   csmaInterfaces = address.Assign(csmaDevices);
-
-  address.SetBase("10.1.3.0", "255.255.255.0");
-  Ipv4InterfaceContainer p2pInterfaces2;
-  p2pInterfaces2 = address.Assign(p2pDevices2);
 
   UdpEchoServerHelper echoServer(9);
 
-  ApplicationContainer serverApps = echoServer.Install(csmaNodes.Get(nCsma));
+  ApplicationContainer serverApps = echoServer.Install(csmaNodes.Get(2));
   serverApps.Start(Seconds(1.0));
   serverApps.Stop(Seconds(10.0));
 
-  UdpEchoClientHelper echoClient(csmaInterfaces.GetAddress(nCsma), 9);
+  UdpEchoClientHelper echoClient(csmaInterfaces.GetAddress(2), 9);
   echoClient.SetAttribute("MaxPackets", UintegerValue(1));
   echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
   echoClient.SetAttribute("PacketSize", UintegerValue(1024));
 
-  ApplicationContainer clientApps1 = echoClient.Install(p2pNodes1.Get(0));
-  clientApps1.Start(Seconds(2.0));
-  clientApps1.Stop(Seconds(10.0));
-
-  ApplicationContainer clientApps2 = echoClient.Install(p2pNodes2.Get(1));
-  clientApps2.Start(Seconds(2.0));
-  clientApps2.Stop(Seconds(10.0));
+  ApplicationContainer clientApps = echoClient.Install(p2pNodes.Get(0));
+  clientApps.Start(Seconds(2.0));
+  clientApps.Stop(Seconds(10.0));
 
   Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-  pointToPoint.EnablePcapAll("second");
-  csma.EnablePcap("second", csmaDevices.Get(1), true);
+  // pointToPoint.EnablePcapAll ("second");
+  // csma.EnablePcap ("second", csmaDevices.Get (1), true);
 
-  AnimationInterface anim("animation.xml");
-  anim.SetConstantPosition(p2pNodes1.Get(0), 0, 5);
-  anim.SetConstantPosition(csmaNodes.Get(0), 10, 5);
-  anim.SetConstantPosition(csmaNodes.Get(1), 20, 5);
-  anim.SetConstantPosition(csmaNodes.Get(2), 30, 5);
-  anim.SetConstantPosition(csmaNodes.Get(3), 40, 5);
-  anim.SetConstantPosition(p2pNodes2.Get(1), 50, 5);
+  AnimationInterface anim("anim_sec.xml");
+  anim.SetConstantPosition(p2pNodes.Get(0), 0, 0);
+  anim.SetConstantPosition(p2pNodes.Get(1), 25.0, 25.0);
+  anim.SetConstantPosition(csmaNodes.Get(1), 50, 50);
+  anim.SetConstantPosition(csmaNodes.Get(2), 75, 75);
   anim.EnablePacketMetadata(true);
 
   Simulator::Run();
